3e562747ecd3769dfd04ae54ad3be8602a5d133b
Bhagya Vinod Rana <bhagyarana2001@gmail.com>
2023-02-18 20:21:50 +0530

    Frontend Phase 1 Commit (#5)
    * Frontend Added
    
    * frontend part 1 added

---
+++ frontend/login/src/components/filelist.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class FileList extends Component {
+  render() {
+    return (
+        <h3>This will Render all File List Objects</h3>
+    )
+  }
+}
