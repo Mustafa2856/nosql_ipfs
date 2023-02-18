e340d5c7eb9c2ce17b80d81f607560452a8e4560
Banseedhar <banseedhargondaliya786@gmail.com>
2023-02-18 13:01:04 +0530

    Database endpoints added

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-import { create } from 'ipfs-http-client';
+const express = require('express');
+const app = express();
 
-const ipfs = create('http://localhost:5001/api/v0');
+app.get('/', (req, res) => {
 
+    res.send('Hello world!!!');
+
+});
+
+// Define middleware function for user validation
+function validateUser(req, res, next) {
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'password') {
+        req.user = { username: 'admin' };
+        next();
+    } else {
+        res.status(401).send('Invalid username or password');
+    }
+}
+
+function validateDataType(dataType) {
+    return function (req, res, next) {
+        if (typeof req.body === dataType) {
+            next();
+        } else {
+            res.status(400).send(`Invalid data type: expected ${dataType}`);
+        }
+    }
+}
+
+app.post('/switchdatabse', validateUser,validateDataType('String'),(req, res) => {
+    const id = req.body.id;
+});
+
+app.post('/insert',validateUser,validateDataType('Object'), (req, res) => {
+
+});
+
+app.post('/findall', validateUser,validateDataType('Object'), (req, res) => {
+
+});
+
+app.post('/filterdata', validateUser,validateDataType('Object'), (req, res) => {
+
+});
+
+app.post('/projectfields', validateUser,validateDataType('Object'),(req, res) => {
+
+});
+app.post('/aggregate', validateUser, validateDataType('Object'), (req, res) => {
+
+});
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
